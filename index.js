55ff472b22a5999c0dc87f906749a1aeda67e518
Raanan Adut <radesigner84@gmail.com>
2024-02-05 22:12:21 +0200

    create docker-compose file and connect redis-server

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379,
+});
 client.set('visits', 0);
 
-app.get('/', (req, res) => {
-  client.get('visits', (err, visits) => {
+app.get('/', async (req, res) => {
+  await client.get('visits', (err, visits) => {
     res.send('Number of visits: ' + visits);
     client.set('visits', parseInt(visits) + 1);
   });
